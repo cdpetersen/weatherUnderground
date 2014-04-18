f7c0f59dc40d22c5d16493d34782cf8b875c9a1e
Nate West <nwest@detroitlabs.com>
2014-04-18 13:35:56 -0400

    add gitignore and gitattributes

--- Pods/Pods-WeatherUnderground-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeatherUnderground : NSObject
-@end
-@implementation PodsDummy_Pods_WeatherUnderground
-@end
--- Pods/Pods-WeatherUndergroundTests-Expecta-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeatherUndergroundTests_Expecta : NSObject
-@end
-@implementation PodsDummy_Pods_WeatherUndergroundTests_Expecta
-@end
