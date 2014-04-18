c111b8f7a91d8b86d366651ccec175dcef76e648
Chris Petersen <chris@detroitlabs.com>
2014-04-18 10:41:21 -0400

    Initial Commit

---
+++ WeatherUnderground/WUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WUAppDelegate.h
+//  WeatherUnderground
+//
+//  Created by Chris Petersen on 4/18/14.
+//  Copyright (c) 2014 Chris Petersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeatherUnderground/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeatherUnderground
+//
+//  Created by Chris Petersen on 4/18/14.
+//  Copyright (c) 2014 Chris Petersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WUAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WUAppDelegate class]));
+    }
+}
